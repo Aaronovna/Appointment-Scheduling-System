7d4e3c257062b4551159b7c09861f791e985140e
pyromagne <growganoth826@gmail.com>
2023-05-05 09:34:27 +0800

    update database

--- ass_db.sql
+++ ass_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 03, 2023 at 01:30 PM
+-- Generation Time: May 05, 2023 at 03:33 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -24,16 +24,32 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `appointments`
+-- Table structure for table `general_appointment`
 --
 
-CREATE TABLE `appointments` (
+CREATE TABLE `general_appointment` (
   `appointment_ID` int(8) NOT NULL,
   `student_ID` int(11) NOT NULL,
+  `type` int(11) NOT NULL,
   `concern` varchar(100) NOT NULL,
   `description` varchar(1000) NOT NULL,
-  `date` date NOT NULL
+  `date` date NOT NULL,
+  `time` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `general_appointment`
+--
+
+INSERT INTO `general_appointment` (`appointment_ID`, `student_ID`, `type`, `concern`, `description`, `date`, `time`) VALUES
+(264650, 32127091, 1, 'Pay Balance', 'Pay Balance', '3030-03-03', '16:00:00'),
+(2341652, 21016980, 3, 'Department', 'Admissions Office', '2023-05-04', '08:00:00'),
+(12196487, 21016980, 2, 'Avail Book', 'Avail Book', '2023-05-04', '08:00:00'),
+(25405993, 32127091, 1, 'Pay Balance', 'Pay Balance', '6969-09-06', '10:00:00'),
+(40194047, 21016980, 1, 'Pay Balance', 'Pay Balance', '2222-02-22', '12:00:00'),
+(60006963, 32127091, 2, 'Avail Book', 'Avail Book', '2023-05-04', '10:00:00'),
+(69707441, 21016980, 1, 'Pay Balance', 'Pay Balance', '2023-02-10', '10:00:00'),
+(78582270, 21016980, 1, 'Pay Balance', 'Pay Balance', '1111-11-11', '08:00:00');
 
 -- --------------------------------------------------------
 
@@ -67,9 +83,9 @@ INSERT INTO `student_account` (`student_ID`, `email`, `password`, `student_numbe
 --
 
 --
--- Indexes for table `appointments`
+-- Indexes for table `general_appointment`
 --
-ALTER TABLE `appointments`
+ALTER TABLE `general_appointment`
   ADD PRIMARY KEY (`appointment_ID`);
 
 --
